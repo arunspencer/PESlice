76d83297a14f53ed640951a56bef9464b452cc3f
Arun Spencer <arunspencer00@gmail.com>
2025-06-03 00:46:13 +0100

    docs: peslice.complexity.chebyshev_complexity - updated docstring

--- src/peslice/complexity/chebyshev_complexity.py
+++ src/peslice/complexity/chebyshev_complexity.py
@@ -11,15 +11,23 @@ def chebyshev_complexity(X: Float[np.ndarray, "N A"],
     max_order: int = 100
 ) -> float:
     """
-    Returns the Chebyshev complexity ....
+    See :class:`ChebyshevModel` for more details
 
     Parameters
     ----------
-    alpha : 
-        Regularisation parameter for the Lasso regression, by default 0.0005
+    alpha
+        Regularisation parameter for the Lasso regression
 
+        Default is 0.0005
     max_order : int
-        Maximum order of the Chebyshev polynomial basis, by default 100
+        Maximum order of the Chebyshev polynomial basis
+
+        Default is 100
+    
+    Returns
+    -------
+    float
+        The Chebyshev complexity of the function y = f(X)
     """
 
     model = ChebyshevModel(alpha=alpha, order=max_order)

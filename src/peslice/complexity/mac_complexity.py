e2f58d652fd4acf78e3a584620e776ed0f848f62
Arun Spencer <arunspencer00@gmail.com>
2025-06-03 00:37:35 +0100

    docs: peslice.complexity.mac_complexity - updated docstring

--- src/peslice/complexity/mac_complexity.py
+++ src/peslice/complexity/mac_complexity.py
@@ -6,6 +6,25 @@ def mac_complexity(
     dx: float,
     normalise: bool = False
 ) -> float:
+    """
+    Calculates the mean absolute curvature (MAC) complexity of a 1D function
+
+    The curvature is defined as the second derivative of the function
+
+    Parameters
+    ----------
+    y
+        1D array of function values
+    dx
+        Discrete spacing between points in the 1D function
+    normalise
+        Whether to normalise the MAC complexity by the standard deviation of the function values
+
+    Returns
+    -------
+    float
+        The mean absolute curvature (MAC) complexity of the 1D function
+    """
     d2x = np.gradient(np.gradient(y, dx), dx)
 
     mac = np.abs(d2x).mean()

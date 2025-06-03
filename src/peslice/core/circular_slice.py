9fef5e782b020d0d489b28a061ae39f4ce2c09b5
Arun Spencer <arunspencer00@gmail.com>
2025-06-03 01:59:41 +0100

    fix: peslice.core.circular_slice - fixed base class inheritance, fixed slice overwrite logic, fixed double definition of gp_eval, updated docstrings

--- src/peslice/core/circular_slice.py
+++ src/peslice/core/circular_slice.py
@@ -4,25 +4,27 @@
 import numpy as np
 import ase
 import torch
-from graph_pes import GraphPESModel
+from peslice.core.base_slice import BaseSlicePES
 from peslice.utils.geometry import normalised
-from peslice.utils.graphs import define_graphs, batch_calculate
+from peslice.utils.graphs import define_graphs
 
 
-class CircularSlicePES:
+class CircularSlicePES(BaseSlicePES):
     """
-    Used to slice a potential energy surface (PES) in 1D by moving each atom in circular paths with bounding radii and angular speeds
+    Slice a potential energy surface (PES) in 1D by moving each atom in circular paths with a random (or constant) radius but random angular speeds and rotation planes
 
-    The 1D slice is defined by moving through time
+    All random parameters are bounded by the parameters defined in the ``def_slice`` method
 
-    PES slices can be evaluted using a parsed `graph-pes` network model
+    The 1D slice is defined by moving through an arbitrary time dimension
+
+    PES slices can be evaluated using a parsed ``graph-pes`` NN model
 
     Built-in PES complexity calculations using Chebyshev and mean absolute curvature (MAC) methods
 
     Parameters
     ----------
     structure
-        The `ase.Atoms` object of the structure being studied
+        The ``ase.Atoms`` object of the structure being studied
     cheb_alpha
         The alpha value for the Lasso regression in the Chebyshev polynomial fitting model
 
@@ -31,46 +33,50 @@ class CircularSlicePES:
     Attributes
     ----------
     structure
-        The `ase.Atoms` object of the structure being studied
-    slice_structures
-        The `ase.Atoms` objects for each sample point along the 1D slice
-    t
-        The time points for each sample point along the 1D slice
-    cheb_score
-        The R^2 score of the Chebyshev fit
-    cheb_complexity
-        The Chebyshev complexity measure of the PES slice - see `chebyshev.py`
-    mac_complexity
-        The mean absolute curvature complexity measure of the PES slice - see `mac_complexity.py`
+        The ``ase.Atoms`` object of the structure being studied
+    graphs
+        The list of graphs representing the slice points, defined by the ``def_slice`` method
     energies
-        The energies of the sample points in the hyperplane calculated by the parsed `graph-pes` model
+        The energies of the sample points in the hyperplane calculated by the parsed ``graph-pes`` model, defined in the ``gp_eval`` method
     forces
-        The atomic forces of the sample points in the hyperplane calculated by the parsed `graph-pes` model
+        The atomic forces of the sample points in the hyperplane calculated by the parsed ``graph-pes`` model, defined in the ``gp_eval`` method
+
+        Default value = None
     abs_forces
-        The absolute atomic forces of the sample points in the hyperplane calculated by the parsed `graph-pes` model
+        The absolute atomic forces of the sample points in the hyperplane calculated by the parsed ``graph-pes`` model, defined in the ``gp_eval`` method
+
+        Default value = None
+    cheb_score
+        The R^2 score of the Chebyshev fit, calculated in the ``cheb_complexity`` property
+    cheb_complexity
+        The Chebyshev complexity measure of the PES slice - ``peslice.complexity.chebyshev_model``
+    mac_complexity
+        The mean absolute curvature complexity measure of the PES slice - ``peslice.complexity.mac_complexity``
+    slice_structures
+        The ``ase.Atoms`` objects for each sample point along the hyperplane
+    persistent_info
+        Whether to keep ``ase.Atoms.info`` attributes for each sample point along the hyperplane
+    t
+        The arbitrary time dimension of the 1D slice, defined by the ``def_slice`` method
     steps
-    The steps along the 1D slice (a list from 1 to the number of steps)
+        The list of steps along the 1D slice, defined by the ``def_slice`` method
 
     Methods
     -------
     def_slice
-        Defines the 1D slice by moving each atom in circular paths with bounding radii and angular speeds
+        Defines a 1D slice by moving each atom in circular paths
+
     gp_eval
-        Evaluates the energy of the `ase.Atoms` objects at each sample point along the 1D slice using a parsed `graph-pes` network model
+        Evaluates the energy and (optionally) forces of the ``ase.Atoms`` objects at each sample point along the 1D slice using a parsed ``graph-pes`` NN model
     """
 
     def __init__(
         self,
         structure: ase.Atoms,
         cheb_alpha: float = 0.0005,
-        overwrite_warning_supressed: bool = False,
         persistent_info: bool = True,
     ):
-        self._defined = False
-        self._evaluated = False
-        self._cheb_alpha = cheb_alpha
-        self.structure = structure
-        self.overwrite_warning_supressed = overwrite_warning_supressed
+        super().__init__(structure=structure, cheb_alpha=cheb_alpha)
         self.persistent_info = persistent_info
         self._get_forces = False
 
@@ -86,7 +92,7 @@ class CircularSlicePES:
         seed: int = 42,
     ) -> None:
         """
-        Defines a 1D slice by moving each atom in circular paths with bounding radii and angular speeds
+        Defines a 1D slice by moving each atom in circular paths with a constant radius but random angular speeds and rotation planes
 
         Parameters
         ----------
@@ -97,11 +103,11 @@ class CircularSlicePES:
         min_radius
             The minimum radius of the circular path
 
-            Default value = 0.01
+            Default value = 0.001
         max_radius
             The maximum radius of the circular path
 
-            Default value = 0.08
+            Default value = 0.2
         constant_radius
             Whether to use the maximum radius as a constant radius for all atoms
 
@@ -119,19 +125,12 @@ class CircularSlicePES:
 
             Default value = 5.0
         seed
-            The seed for the random generation of the slice
+            The seed for random number generation
 
             Default value = 42
         """
-        # checks if a slice is already defined - overwrites
+        # checks if a slice is already defined
         if self._defined:
-            if not self.overwrite_warning_supressed:
-                print("Overwriting previous slice")
-            self._evaluated = False
-            self.t = np.zeros_like(self.t)
-
-        # checks if the slice is already evaluated - removes previous evaluations
-        if self._evaluated:
             self._evaluated = False
 
         R = torch.tensor(self.structure.positions)
@@ -214,57 +213,8 @@ class CircularSlicePES:
         self.steps = [i + 1 for i in range(n_steps)]
         self._defined = True
 
-    def gp_eval(
-        self,
-        model: GraphPESModel,
-        get_forces: bool = False,
-        batch_size: int = 10,
-    ) -> None:
-        """
-        Evaluates the energy, and optionally forces, of the ase.Atoms objects at each sample point along the 1D slice
-
-        Parameters
-        ----------
-        model
-            The `graph-pes` network model class instance to use
-        get_forces
-            Whether to calculate the forces at each sample point
-
-            Default value = False
-        batch_size
-            The batch size to use for the evaluation of energies and (optionally) forces
-
-            Default value = 10
-
-        Raises
-        ------
-        ValueError
-            If the slice has not yet been defined
-        """
-        if not self._defined:
-            raise ValueError("Slice must be defined before it can be evaluated")
-
-        energies, forces, abs_forces = batch_calculate(
-            model, self.graphs, get_forces=get_forces, batch_size=batch_size
-        )
-
-        self.energies = energies
-        self.forces = forces
-        self.abs_forces = abs_forces
-        self._evaluated = True
-
     def _cheb_input(self) -> np.ndarray:
         return np.array(self.t)
     
     def _mac_dx(self) -> float:
         return float(self.t[-1]) / len(self.t)
-    
-    def __repr__(self) -> str:
-        return (
-            "TODO"
-        )
-
-    def summary(self) -> str:
-        return (
-            "TODO"
-        )

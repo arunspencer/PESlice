01f6941927a430544e15f187b6a8136f0b7e0c06
Arun Spencer <arunspencer00@gmail.com>
2025-06-03 01:38:54 +0100

    fix: peslice.core.linear_slice - fixed double definition of gp_eval, fixed logic bug with nn distance calculation, removed slice overwrite warning, updated docstrings

--- src/peslice/core/linear_slice.py
+++ src/peslice/core/linear_slice.py
@@ -4,32 +4,32 @@
 import numpy  as np
 import ase
 from jaxtyping import Float
-from graph_pes import GraphPESModel
 from peslice.core.base_slice import BaseSlicePES
 from peslice.utils.geometry import gen_random_slice, check_orthogonal
-from peslice.utils.graphs import define_graphs, batch_calculate, get_nearest_neighbour_distance
+from peslice.utils.graphs import define_graphs, get_nearest_neighbour_distance
 
 class LinearSlicePES(BaseSlicePES):
     """
-    Used to slice a potential energy surface (PES) along a n-dimensional hyperplane (n=1, 2, 3)
+    Slice a potential energy surface (PES) along a n-dimensional hyperplane (n = 1, 2, 3)
 
-    Hyperplanes are defined by a point and n-1 basis vectors (the vectors contain orthogonal subvectors of length 3 - corresponding to atomic (x, y, z) coordinates)
+    Hyperplanes are defined by a point and ``n-1`` basis vectors
+    
+    Vectors contain orthogonal sub-vectors of length 3 - corresponding to atomic Cartesian coordinates
 
-    PES slices can be evaluted using a parsed `graph-pes` network model
+    PES slices can be evaluated using a parsed ``graph-pes`` NN model
 
-    The hyperplane is defined by the equation: y = m0 + c1*m1 + c2*m2 + c3*m3 + ..., such that atomic positions can be generated through the below equation
+    The hyperplane is defined by the equation: ``y = m0 + c1*m1 + c2*m2 + c3*m3 + ...``
 
-    Atomic positions: p(c1, c2, c3, ...) = p0 + c1*m1 + c2*m2 + c3*m3 + ...
+    Atomic positions: ``p(c1, c2, c3, ...) = p0 + c1*m1 + c2*m2 + c3*m3 + ...``
 
     The coefficients have units of Angstroms (Å)
 
     Built-in PES complexity calculations using Chebyshev and mean absolute curvature (MAC) methods
 
-
     Parameters
     ----------
     structure
-        The `ase.Atoms` object of the structure being studied
+        The ``ase.Atoms`` object of the structure being studied
     point_density
         The number of points to sample along each dimension in the hyperplane per unit length
 
@@ -38,59 +38,67 @@ class LinearSlicePES(BaseSlicePES):
         The alpha value for the Lasso regression in the Chebyshev polynomial fitting model
 
         Default value = 0.0005
+    get_nn_distances
+        Whether to calculate the smallest nearest neighbour distances for each sample point along the hyperplane
+
+        Default value = False
 
     Attributes
     ----------
+    structure
+        The ``ase.Atoms`` object of the structure being studied
+    graphs
+        The list of graphs representing the slice points, defined by the ``def_slice`` method
+    energies
+        The energies of the sample points in the hyperplane calculated by the parsed ``graph-pes`` model, defined in the ``gp_eval`` method
+    forces
+        The atomic forces of the sample points in the hyperplane calculated by the parsed ``graph-pes`` model, defined in the ``gp_eval`` method
+
+        Default value = None
+    abs_forces
+        The absolute atomic forces of the sample points in the hyperplane calculated by the parsed ``graph-pes`` model, defined in the ``gp_eval`` method
+
+        Default value = None
+    cheb_score
+        The R^2 score of the Chebyshev fit, calculated in the ``cheb_complexity`` property
+    cheb_complexity
+        The Chebyshev complexity measure of the PES slice - ``peslice.complexity.chebyshev_model``
+    mac_complexity
+        The mean absolute curvature complexity measure of the PES slice - ``peslice.complexity.mac_complexity``
     function_dim
         The dimension of the function describing the PES
     point_density
         The number of points to sample along each dimension in the hyperplane per unit length
     ranges_n
-        The value of n to define [-n, n] ranges for the plane coefficients
+        The value of n to define the range ``[-n, n]`` to sample the plane coefficients
     slice_dim
         The dimension of the hyperplane
     m0
         A point on the hyperplane
 
-        Shape `(function_dim, )`
+        Shape ``(function_dim, )``
     orthogonal_basis
-        The orthogonal basis vectors that define the hyperplane `(m1, m2, m3, ...)`
+        The orthogonal basis vectors that define the hyperplane ``(m1, m2, m3, ...)``
 
-        `y = m0 + c1*m1 + c2*m2 + c3*m3 + ...`
-
-        Shape `(slice_dim, function_dim)`
+        Shape ``(slice_dim, function_dim)``
     coefficients
-        The values of `(c1, c2, c3, ...)` that define the sample points in the hyperplane
+        The values of ``(c1, c2, c3, ...)`` that define the sample points in the hyperplane
 
-        Shape `(num_points, slice_dim)`
+        Shape ``(num_points, slice_dim)``
 
-        `num_points` is the product of the number of points in each dimension, determined by the `point_density` parameter
-    structure
-        The `ase.Atoms` object of the structure being studied
+        ``num_points`` is the product of the number of points in each dimension, determined by the ``point_density`` parameter
     slice_structures
-        The `ase.Atoms` objects for each sample point along the hyperplane
-    graphs
-        The `graph_pes.AtomicGraph` objects for each sample point along the hyperplane
-    complexity
-        The Chebyshev complexity measure of the PES slice - see `chebyshev.py`
-    cheb_score
-        The R^2 score of the Chebyshev fit
-    energies
-        The energies of the sample points in the hyperplane calculated by the parsed `graph-pes` model
-    forces
-        The atomic forces of the sample points in the hyperplane calculated by the parsed `graph-pes` model
-    abs_forces
-        The absolute atomic forces of the sample points in the hyperplane calculated by the parsed `graph-pes` model
+        The ``ase.Atoms`` objects for each sample point along the hyperplane
     nn_neighbour_distances
         The nearest nearest neighbour distance for each sample point along the hyperplane
     Methods
     -------
     def_slice
-        Defines the hyperplane in `slice_dim`-dimensions by parsing orthogonal basis vectors
+        Defines the hyperplane in ``slice_dim``-dimensions by parsing orthogonal basis vectors
     def_rslice
-        Defines a random hyperplane in `slice_dim`-dimensions
+        Defines a random hyperplane in ``slice_dim``-dimensions
     gp_eval
-        Evaluates the function at the sample points in the hyperplane using a parsed `graph-pes` many-body model
+        Evaluates the function at the sample points in the hyperplane using a parsed ``graph-pes`` many-body model
     """
 
     def __init__(
@@ -113,28 +121,28 @@ class LinearSlicePES(BaseSlicePES):
         ranges_n: float = 0.9,
     ) -> None:
         """
-        Defines the hyperplane in n-dimensions (n=1, 2, 3)
+        Defines the hyperplane in n-dimensions (n = 1, 2, 3)
 
         Parameters
         ----------
         m0
             A point on the hyperplane
 
-            Must have `dtype=np.float64`
+            Must have ``dtype=np.float64``
 
-            Shape `(function_dim, )`
+            Shape ``(function_dim, )``
         *args
-            Orthogonal basis vectors that define the hyperplane `(m1, m2, m3, ..., mA)`
+            Orthogonal basis vectors that define the hyperplane ``(m1, m2, m3, ..., mA)``
 
-            Must have `dtype=np.float64`
+            Must have ``dtype=np.float64``
 
-            Shape `(function dim, )`
+            Shape ``(function_dim, )``
         model_cutoff
-            The cutoff distance for the graph-pes model
+            The cutoff distance for the parsed ``graph-pes`` model
 
             Default value = 5.0
         ranges_n
-            The value of n to define [-n, n] ranges for the plane coefficients
+            The value of n to define the range ``[-n, n]`` to sample the plane coefficients
 
             Default value = 0.9
 
@@ -149,9 +157,9 @@ class LinearSlicePES(BaseSlicePES):
 
             If the length of the vector is not (3*number of atoms) - atomic (x, y, z) coordinates
 
-            If the vectors do not have `dtype=np.float64`
+            If the vectors do not have ``dtype=np.float64``
 
-            If m0 does not have `dtype=np.float64`
+            If m0 does not have ``dtype=np.float64``
         """
         # checks that ranges_n is > 0
         if ranges_n <= 0:
@@ -219,12 +227,13 @@ class LinearSlicePES(BaseSlicePES):
         self._defined = True
 
         # get nearest neighbour distances
-        nn_neighbour_distances = []
-        for graph in self.graphs:
-            nn_neighbour = get_nearest_neighbour_distance(graph)
-            nn_neighbour_distances.append(nn_neighbour)
+        if self.get_nn_distances:
+            nn_neighbour_distances = []
+            for graph in self.graphs:
+                nn_neighbour = get_nearest_neighbour_distance(graph)
+                nn_neighbour_distances.append(nn_neighbour)
 
-        self.nn_neighbour_distances = nn_neighbour_distances
+            self.nn_neighbour_distances = nn_neighbour_distances
 
     def def_rslice(
         self,
@@ -234,20 +243,22 @@ class LinearSlicePES(BaseSlicePES):
         ranges_n: float = 0.9,
     ) -> None:
         """
-        Defines a random hyperplane in `slice-dim`-dimensions.
+        Defines a random hyperplane in ``slice-dim``-dimensions.
 
         Parameters
         ----------
         slice_dim
             The dimension of the hyperplane
         model_cutoff
-            The cutoff distance for the graph-pes model
+            The cutoff distance for the parsed ``graph-pes`` model
 
             Default value = 5.0
         seed
-            The seed for the random generation of slices
+            The seed for random number generation
+
+            Default value = 42
         ranges_n
-            The value of n to define [-n, n] ranges for the plane coefficients
+            The value of n to define the range ``[-n, n]`` to sample the plane coefficients
 
             Default value = 0.9
 
@@ -258,7 +269,7 @@ class LinearSlicePES(BaseSlicePES):
 
             If ranges_n is <= 0
 
-            If m0 does not have `dtype=np.float64`
+            If m0 does not have ``dtype=np.float64``
         """
 
         m0 = self.structure.positions.reshape(-1)
@@ -283,13 +294,9 @@ class LinearSlicePES(BaseSlicePES):
         if m0.dtype != np.float64:
             raise ValueError("m0 must have `dtype=np.float64`")
 
-        # overwrites the previous slice if it is already defined
+        # checks if a slice is already defined
         if self._defined:
-            print("Overwriting previous slice")
             self._evaluated = False
-            self.energies = np.zeros_like(self.energies)
-            if self._get_forces:
-                self.forces = np.zeros_like(self.forces)
 
         # creates orthogonal basis vectors
         orthogonal_basis = gen_random_slice(m0, slice_dim, seed)
@@ -323,7 +330,7 @@ class LinearSlicePES(BaseSlicePES):
 
     def _generate_points(self) -> None:
         """
-        Generates sample points in the hyperplane from the orthogonal basis vectors, `ranges_n`, and `point_density` (in each dimension)
+        Generates sample points in the hyperplane from the orthogonal basis vectors, ``ranges_n``, and ``point_density``
         """
         slice_dim = self.slice_dim
         ranges_n = self.ranges_n
@@ -343,6 +350,9 @@ class LinearSlicePES(BaseSlicePES):
         self.coefficients = coefficients
 
     def _generate_atoms(self) -> list[ase.Atoms]:
+        """
+        Generates the ``ase.Atoms`` objects for each sample point along the hyperplane
+        """
         molecules = []  # to store an Atoms object for each sample point
 
         # generating new atomic positions for each sample point, creating a new Atoms object
@@ -370,65 +380,8 @@ class LinearSlicePES(BaseSlicePES):
 
         return molecules
 
-    def gp_eval(
-        self,
-        model: GraphPESModel,
-        get_forces: bool = False,
-        batch_size: int = 10,
-    ) -> None:
-        """
-        Evaluates the energy, and optionally forces, of the ase.Atoms objects at each sample point along the hyperplane
-
-        Parameters
-        ----------
-        model
-            The `graph-pes` network model class instance to use
-        get_forces
-            Whether to calculate the forces at each sample point
-
-            Default value = False
-        batch_size
-            The batch size to use for the evaluation of energies and (optionally) forces
-
-            Default value = 10
-
-        Raises
-        ------
-        ValueError
-            If the slice has not yet been defined
-
-            If the number of atomic directions in the structure does not match the function dimension
-        """
-        if not self._defined:
-            raise ValueError("Slice must be defined before it can be evaluated")
-
-        if self._evaluated:
-            self._evaluated = False
-
-        self._get_forces = get_forces
-
-        energies, forces, abs_forces = batch_calculate(
-            model, self.graphs, batch_size=batch_size, get_forces=get_forces
-        )
-
-        self.energies = energies  # shape (num_points, )
-        self.forces = forces
-        self.abs_forces = abs_forces
-
-        self._evaluated = True
-
     def _cheb_input(self) -> np.ndarray:
         return self.coefficients
     
     def _mac_dx(self) -> float:
         return 1/self.point_density
-    
-    def __repr__(self) -> str:
-        return (
-            "TODO"
-        )
-
-    def summary(self) -> str:
-        return (
-            "TODO"
-        )
